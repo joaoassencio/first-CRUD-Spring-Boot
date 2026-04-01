521db095b7bc4664b3e040c8fa3ded5dd6e12b7b
João Assêncio <joao.assencio123@gmail.com>
2026-04-01 12:33:22 -0300

    feat: Criado o UserRepository, perfil de testes e injeção de dependência para converter objetos criados manualmente em dados para popular o banco de dados automaticamente ao iniciar a aplicação

---
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -0,0 +1,40 @@
+package com.educandoweb.course.config;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User u1 = new  User(
+                null,
+                "João",
+                "joao@email.com",
+                "61 99999-9999",
+                "@Dm1n"
+        );
+
+        User u2 = new  User(
+                null,
+                "Jéssica",
+                "jessica@email.com",
+                "61 98888-8888",
+                "M1nh@"
+        );
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}
